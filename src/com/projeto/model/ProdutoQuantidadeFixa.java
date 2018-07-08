b744b21deb7cccb3d081b7753492adf3f39a662a
paulojuniore <paulo.junior@ccc.ufcg.edu.br>
2018-07-08 18:47:13 -0300

    Iniciando produto com quantidade fixa

--- src/com/projeto/model/ProdutoQuantidadeFixa.java
+++ src/com/projeto/model/ProdutoQuantidadeFixa.java
@@ -1,11 +1,38 @@
 package com.projeto.model;
 
-public class ProdutoQuantidadeFixa extends Item{
+public class ProdutoQuantidadeFixa extends Item {
 	
-	public ProdutoQuantidadeFixa(int id, String nome, String categoria) {
+	private double quilo;
+	
+	private String localCompra;
+	
+	private Object preco;
+	
+	
+	public ProdutoQuantidadeFixa(int id, String nome, String categoria, double quilo, String localCompra, 
+			double preco) {
 		super(id, nome, categoria);
-		// TODO Criar e inicializar os métodos relacionados a classe ...
+		this.quilo = quilo;
+		this.localCompra = localCompra;
+		this.preco = preco;
 	}
-
+	
+	public double getQuilo() {
+		return quilo;
+	}
+	
+	public String localCompra() {
+		return localCompra;
+	}
+	
+	public Object getPreco() {
+		return preco;
+	}
+	
+	@Override
+	public String toString() {
+		return this.getId() + ". " + this.getNome() + ", " + this.getCategoria()
+		+ ", Preco: " + this.getPreco() + "<AINDA FALTA IMPLEMENTAR>";
+	}
 
 }
