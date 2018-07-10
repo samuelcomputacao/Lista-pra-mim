fa0e7b3cad0a0c3676a6b93c2ff50ca7553b7f83
igorsp <igorsp@lcc2-29.lcc.ufcg.edu.br>
2018-07-10 15:39:50 -0300

    Alguns ajustes

--- src/com/projeto/comparadores/ComparaValor.java
+++ src/com/projeto/comparadores/ComparaValor.java
@@ -4,11 +4,16 @@ import java.util.Comparator;
 
 import com.projeto.model.Item;
 
-public class ComparaValor implements Comparator<Item>{
-
+/**
+ * Classe Voltada para a comparação de dois itens atravez do seu menor preco
+ */
+public class ComparaValor implements Comparator<Item> {
+	/**
+	 * Metodo voltado para a comparacao de dois itens atravez dos seus precos
+	 */
 	@Override
 	public int compare(Item arg0, Item arg1) {
-		if(arg0.getMenorPreco() == arg1.getMenorPreco())
+		if (arg0.getMenorPreco() == arg1.getMenorPreco())
 			return 0;
 		else if (arg0.getMenorPreco() > arg1.getMenorPreco())
 			return 1;
