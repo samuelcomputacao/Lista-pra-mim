33a9ad16af816864ddd0b9284eef040f82364abb
pereiraIgor <igor.pereira@ccc.ufcg.edu.br>
2018-07-12 10:54:18 -0300

    Documentacao atualizada

--- unity_tests/com/projeto/model/ProdutoPorUnidadeTest.java
+++ unity_tests/com/projeto/model/ProdutoPorUnidadeTest.java
@@ -8,13 +8,13 @@ import org.junit.Test;
 import com.projeto.excecoes.CampoInvalidoException;
 
 /**
- * Classe responsavel por testar a classe Produto por Unidade
+ * Classe responsavel por testar a classe Produto por Unidade.
  */
 public class ProdutoPorUnidadeTest {
 	private ProdutoPorUnidade produtoPorUnidade;
 
 	/**
-	 * Inicializa o ProdutoPorUnidade
+	 * Inicializa o ProdutoPorUnidade.
 	 */
 	@Before
 	public void testIniciaProdutoPorUnidade() {
@@ -47,7 +47,7 @@ public class ProdutoPorUnidadeTest {
 	}
 
 	/**
-	 * Testa o levantamento de erro atraves de um nome vazio
+	 * Testa o levantamento de erro atraves de um nome vazio.
 	 */
 	@Test(expected = CampoInvalidoException.class)
 	public void testProdutoPorUnidadeComNomeVazio() {
@@ -55,21 +55,22 @@ public class ProdutoPorUnidadeTest {
 	}
 
 	/**
-	 * Testa o levantamento de erro atraves da categoria com valor null
+	 * Testa o levantamento de erro atraves da categoria com valor null.
 	 */
 	public void testProdutoPorUnidadeComCategoriaNull() {
 		produtoPorUnidade = new ProdutoPorUnidade(2, "Cha de linho", null, 2, "Ervas & CIA", 4.20);
 	}
 
 	/**
-	 * Testa o levantamento de erro atraves da categoria com valor vazio
+	 * Testa o levantamento de erro atraves da categoria com valor vazio.
 	 */
 	public void testProdutoPorUnidadeComCategoriaVazia() {
 		produtoPorUnidade = new ProdutoPorUnidade(2, "Cha de linho", "  ", 2, "Ervas & CIA", 4.20);
 	}
 
 	/**
-	 * Testa o levantamento de erro atraves da quantidade de unidades abaixo de zero
+	 * Testa o levantamento de erro atraves da quantidade de unidades abaixo de
+	 * zero.
 	 */
 	@Test(expected = CampoInvalidoException.class)
 	public void testProdutoPorUnidadeComUnidadeAbaixoZero() {
@@ -77,7 +78,7 @@ public class ProdutoPorUnidadeTest {
 	}
 
 	/**
-	 * Testa o levantamento de erro atraves do preco abaixo de zero
+	 * Testa o levantamento de erro atraves do preco abaixo de zero.
 	 */
 	@Test(expected = CampoInvalidoException.class)
 	public void testProdutoPorUnidadeComPrecoAbaixoZero() {
@@ -85,27 +86,92 @@ public class ProdutoPorUnidadeTest {
 				-20.43);
 	}
 
+	/**
+	 * Metodo responsavel por adicionar locais de compra para o atual produto.
+	 */
 	@Test
 	public void testAdicionarLocalCompra() {
 		this.produtoPorUnidade.adicionarLocalCompra("Cacau xou", 60.0);
+		this.produtoPorUnidade.adicionarLocalCompra("PharmaLive", 20.21);
+		this.produtoPorUnidade.adicionarLocalCompra("MachFarma", 5.99);
 	}
-	@Test(expected= CampoInvalidoException.class)
+
+	/**
+	 * Metodo responsavel por detectar um erro ao adicionar um local de compra com o
+	 * nome vazio.
+	 */
+	@Test(expected = CampoInvalidoException.class)
 	public void testAdicionarLocalCompraNomeVazio() {
 		this.produtoPorUnidade.adicionarLocalCompra("   ", 60.0);
 	}
 
-	@Test(expected= CampoInvalidoException.class)
-	public void testAdicionarLocalCompraValorAbaixoZero() {
-		this.produtoPorUnidade.adicionarLocalCompra("Matins&Co", -502.0);
-	}
-	
-	@Test(expected= CampoInvalidoException.class)
+	/**
+	 * Metodo responsavel por detectar um erro em adicionar um local de compra com
+	 * nome vazio.
+	 */
+	@Test(expected = CampoInvalidoException.class)
 	public void testAdicionarLocalCompraNomeNull() {
 		this.produtoPorUnidade.adicionarLocalCompra(null, 60.0);
 	}
-	
+
 	/**
-	 * Testa a igualdade do getUnidade
+	 * Metodo responsavel por detectar um erro atravez de adicionar um local de
+	 * compra com valor 0.
+	 */
+	@Test(expected = CampoInvalidoException.class)
+	public void testAdicionarLocalCompraValorZero() {
+		this.produtoPorUnidade.adicionarLocalCompra("Matins&Co", 0.0);
+	}
+
+	/**
+	 * Metodo responsavel por detectar um erro atravez de adicionar um local de
+	 * compra com valor abaixo de 0.
+	 */
+	@Test(expected = CampoInvalidoException.class)
+	public void testAdicionarLocalCompraValorAbaixoZero() {
+		this.produtoPorUnidade.adicionarLocalCompra("Matins&Co", -502.0);
+	}
+
+	/**
+	 * Metodo responsavel pela analise da igualdade entre 3 produtos.
+	 */
+	@Test
+	public void testIgualdadeProdutoPorUnidade() {
+		ProdutoPorUnidade produtoPorUnidade2 = new ProdutoPorUnidade(2, "sabonete", "higiene pessoal", 1, "uaumart",
+				5.7);
+		ProdutoPorUnidade produtoPorUnidade3 = new ProdutoPorUnidade(1, "Pasta Dental", "higiene pessoal", 1, "Lamarck",
+				13.95);
+		assertFalse(produtoPorUnidade2.equals(produtoPorUnidade));
+		assertTrue(produtoPorUnidade3.equals(produtoPorUnidade));
+	}
+
+	/**
+	 * Metodo responsavel pela verificacao do getCategoria do produtoPorUnidade.
+	 */
+	@Test
+	public void testGetCategoria() {
+		assertEquals(this.produtoPorUnidade.getCategoria(), "higiene pessoal");
+	}
+
+	/**
+	 * Metodo responsavel pela verificacao do getNome do produtoPorUnidade.
+	 */
+	@Test
+	public void testGetNome() {
+		assertEquals(this.produtoPorUnidade.getNome(), "Pasta Dental");
+	}
+
+	/**
+	 * Metodo responsavel pela verificacao do getListaPrecos do produtoPorUnidade.
+	 */
+	@Test
+	public void testGetListaPrecos() {
+		this.produtoPorUnidade.adicionarLocalCompra("Pharma raia", 8.2);
+		assertEquals(this.produtoPorUnidade.getListaPrecos(), "<Rede pharma, R$ 13,95;Pharma raia, R$ 8,20;>");
+	}
+
+	/**
+	 * Testa a igualdade do getUnidade.
 	 */
 	@Test
 	public void testProdutoPorUnidade1() {
--- unity_tests/com/projeto/model/ProdutoQuantidadeFixaTest.java
+++ unity_tests/com/projeto/model/ProdutoQuantidadeFixaTest.java
@@ -19,104 +19,119 @@ public class ProdutoQuantidadeFixaTest {
 	 * Produto utilizado como base para os testes.
 	 */
 	private ProdutoQuantidadeFixa produto;
-	
+
 	/**
-	 * Testa o construtor da classe ProdutoQuantidadeFixa com todos os seus parametros válidos.
+	 * Testa o construtor da classe ProdutoQuantidadeFixa com todos os seus
+	 * parametros válidos.
 	 */
 	@Before
 	@Test
 	public void testProdutoQuantidadeFixa() {
-		produto = new ProdutoQuantidadeFixa(1, "Feijão Boa Safra", "alimento nao industrializado", 10, "kg", "Hiper Bom Preço", 4.99);
+		produto = new ProdutoQuantidadeFixa(1, "Feijão Boa Safra", "alimento nao industrializado", 10, "kg",
+				"Hiper Bom Preço", 4.99);
 	}
-	
+
 	/**
-	 * Testa o construtor da classe ProdutoQuantidadeFixa com o nome do produto null. (O que nao e permitido!)
+	 * Testa o construtor da classe ProdutoQuantidadeFixa com o nome do produto
+	 * null. (O que nao e permitido!)
 	 */
 	@Test(expected = CampoInvalidoException.class)
 	public void testProdutoQuantidadeFixaNomeNull() {
 		new ProdutoQuantidadeFixa(1, null, "higiene pessoal", 5, "l", "Mercadinho São José", 10.00);
 	}
-	
+
 	/**
-	 * Testa o construtor da classe ProdutoQuantidadeFixa com o nome do produto vazio. (O que nao e permitido!)
+	 * Testa o construtor da classe ProdutoQuantidadeFixa com o nome do produto
+	 * vazio. (O que nao e permitido!)
 	 */
 	@Test(expected = CampoInvalidoException.class)
 	public void testProdutoQuantidadeFixaNomeVazio() {
 		new ProdutoQuantidadeFixa(1, "  ", "alimento industrializado", 10, "kg", "Atacadão", 4.75);
 	}
-	
+
 	/**
-	 * Testa o construtor da classe ProdutoQuantidadeFixa com a categoria do produto nula.
+	 * Testa o construtor da classe ProdutoQuantidadeFixa com a categoria do produto
+	 * nula.
 	 */
 	@Test(expected = CampoInvalidoException.class)
 	public void testProdutoQuantidadeFixaCategoriaNula() {
 		new ProdutoQuantidadeFixa(1, "Arroz Chinês", null, 5, "kg", "Assaí", 3.75);
 	}
-	
+
 	/**
-	 * Testa o construtor da classe ProdutoQuantidadeFixa com a categoria do produto vazia. 
+	 * Testa o construtor da classe ProdutoQuantidadeFixa com a categoria do produto
+	 * vazia.
 	 */
 	@Test(expected = CampoInvalidoException.class)
 	public void testProdutoQuantidadeFixaCategoriaVazia() {
 		new ProdutoQuantidadeFixa(1, "Macarrão espaguete", "  ", 3, "kg", "Hiper Bom Preço", 3.99);
 	}
-	
+
 	/**
-	 * Testa o construtor da classe ProdutoQuantidadeFixa com a categoria inexistente.
+	 * Testa o construtor da classe ProdutoQuantidadeFixa com a categoria
+	 * inexistente.
 	 */
 	@Test(expected = CategoriaInexistenteException.class)
 	public void testProdutoQuantidadeFixaCategoriaInexistente() {
 		new ProdutoQuantidadeFixa(1, "Carne de Sol", "alimento", 5, "kg", "Assaí", 10.99);
 	}
-	
+
 	/**
 	 * Testa o construtor da classe ProdutoQuantidadeFixa com a quantidade zerada.
 	 */
 	@Test(expected = CampoInvalidoException.class)
 	public void testProdutoQuantidadeFixaQuantidadeNula() {
-		new ProdutoQuantidadeFixa(1, "Salsicha Perdigão", "alimento industrializado", 0, "kg", "Mercadinho Preço Bom", 3.49);
+		new ProdutoQuantidadeFixa(1, "Salsicha Perdigão", "alimento industrializado", 0, "kg", "Mercadinho Preço Bom",
+				3.49);
 	}
-	
+
 	/**
 	 * Testa o construtor da classe ProdutoQuantidadeFixa com a quantidade negativa.
 	 */
 	@Test(expected = CampoInvalidoException.class)
 	public void testProdutoQuantidadeFixaQuantidadeNegativa() {
-		new ProdutoQuantidadeFixa(1, "Salsicha Perdigão", "alimento industrializado", -10, "kg", "Mercadinho Preço Bom", 3.49);
+		new ProdutoQuantidadeFixa(1, "Salsicha Perdigão", "alimento industrializado", -10, "kg", "Mercadinho Preço Bom",
+				3.49);
 	}
-	
+
 	/**
-	 * Testa o construtor da classe ProdutoQuantidadeFixa com a unidade de medida nula.
+	 * Testa o construtor da classe ProdutoQuantidadeFixa com a unidade de medida
+	 * nula.
 	 */
 	@Test(expected = CampoInvalidoException.class)
 	public void testProdutoQuantidadeFixaUnidadeMedidaNula() {
-		new ProdutoQuantidadeFixa(1, "Salsicha Perdigão", "alimento industrializado", 10, null, "Mercadinho Preço Bom", 3.49);
+		new ProdutoQuantidadeFixa(1, "Salsicha Perdigão", "alimento industrializado", 10, null, "Mercadinho Preço Bom",
+				3.49);
 	}
-	
+
 	/**
-	 * Testa o construtor da classe ProdutoQuantidadeFixa com a unidade de medida vazia.
+	 * Testa o construtor da classe ProdutoQuantidadeFixa com a unidade de medida
+	 * vazia.
 	 */
 	@Test(expected = CampoInvalidoException.class)
 	public void testProdutoQuantidadeFixaUnidadeMedidaVazia() {
-		new ProdutoQuantidadeFixa(1, "Carne de Sol", "alimento industrializado", 5, "  ", "Atacadão Rio do Peixe", 14.49);
+		new ProdutoQuantidadeFixa(1, "Carne de Sol", "alimento industrializado", 5, "  ", "Atacadão Rio do Peixe",
+				14.49);
 	}
-	
+
 	/**
-	 * Testa o construtor da classe ProdutoQuantidadeFixa com o local de compra nulo.
+	 * Testa o construtor da classe ProdutoQuantidadeFixa com o local de compra
+	 * nulo.
 	 */
 	@Test(expected = CampoInvalidoException.class)
 	public void testProdutoQuantidadeFixaLocalDeCompraNulo() {
 		new ProdutoQuantidadeFixa(1, "Carne de Charque", "alimento industrializado", 5, "kg", null, 7.50);
 	}
-	
+
 	/**
-	 * Testa o construtor da classe ProdutoQuantidadeFixa com o local de compra vazio.
+	 * Testa o construtor da classe ProdutoQuantidadeFixa com o local de compra
+	 * vazio.
 	 */
 	@Test(expected = CampoInvalidoException.class)
 	public void testProdutoQuantidadeFixaLocalDeCompraVazio() {
 		new ProdutoQuantidadeFixa(1, "Detergente Ypê", "limpeza", 2, "l", "   ", 3.00);
 	}
-	
+
 	/**
 	 * Testa o construtor da classe ProdutoQuantidadeFixa com o preco negativo.
 	 */
@@ -124,7 +139,7 @@ public class ProdutoQuantidadeFixaTest {
 	public void testProdutoQuantidadeFixaPrecoNegativo() {
 		new ProdutoQuantidadeFixa(1, "Desinfetante Ypê", "limpeza", 2, "l", "Hiper Bom Preço", -100.2);
 	}
-	
+
 	/**
 	 * Testa o construtor da classe ProdutoQuantidadeFixa com o preco igual a zero.
 	 */
@@ -132,15 +147,16 @@ public class ProdutoQuantidadeFixaTest {
 	public void testProdutoQuantidadeFixaPrecoNulo() {
 		new ProdutoQuantidadeFixa(1, "Desinfetante Ypê", "limpeza", 2, "l", "Hiper Bom Preço", 0);
 	}
-	
+
 	/**
 	 * Testa a representação String de um ProdutoQuantidadeFixa.
 	 */
 	@Test
 	public void testToString() {
-		assertEquals("1. Feijão Boa Safra, alimento nao industrializado, 10 kg, Preco: <Hiper Bom Preço, R$ 4,99;>", produto.toString());
+		assertEquals("1. Feijão Boa Safra, alimento nao industrializado, 10 kg, Preco: <Hiper Bom Preço, R$ 4,99;>",
+				produto.toString());
 	}
-	
+
 	/**
 	 * Testa o metodo que retorna a quantidade de um produto.
 	 */
@@ -194,7 +210,7 @@ public class ProdutoQuantidadeFixaTest {
 	public void testAdicionarLocalCompraVazio() {
 		produto.adicionarLocalCompra("   ", 10.1);
 	}
-	
+
 	/**
 	 * Testa o metodo adicionarLocalCompra com parametro localCompra nulo.
 	 */
@@ -202,13 +218,15 @@ public class ProdutoQuantidadeFixaTest {
 	public void testAdicionarLocalCompraNull() {
 		produto.adicionarLocalCompra(null, 10.1);
 	}
-	
+
 	/**
-	 * Testa o metodo equals, dois ProdutoQuantidadeFixa sao iguais se possuem mesmo nome e categoria.
+	 * Testa o metodo equals, dois ProdutoQuantidadeFixa sao iguais se possuem mesmo
+	 * nome e categoria.
 	 */
 	@Test
 	public void testEqualsObject() {
-		ProdutoQuantidadeFixa produto2 = new ProdutoQuantidadeFixa(2, "Feijão Boa Safra", "alimento nao industrializado", 2, "kg", "Carrefour", 3.50);
+		ProdutoQuantidadeFixa produto2 = new ProdutoQuantidadeFixa(2, "Feijão Boa Safra",
+				"alimento nao industrializado", 2, "kg", "Carrefour", 3.50);
 		assertEquals(produto, produto2);
 	}
 
@@ -247,7 +265,8 @@ public class ProdutoQuantidadeFixaTest {
 	}
 
 	/**
-	 * Testa o metodo getListaPrecos que mostra uma lista dos locais de compra que possuem determinado produto e seus respectivos valores.
+	 * Testa o metodo getListaPrecos que mostra uma lista dos locais de compra que
+	 * possuem determinado produto e seus respectivos valores.
 	 */
 	@Test
 	public void testGetListaPrecos() {
