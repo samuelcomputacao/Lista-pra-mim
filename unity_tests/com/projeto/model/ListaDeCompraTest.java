78211bff60f45d077f9927a79f1309049113b1f5
Viniciuswps <vinicius.barbosa.silva@ccc.ufcg.edu.br>
2018-07-27 17:50:43 -0300

    add Estabelecimento/ListaDeCompra Test não implementados

---
+++ unity_tests/com/projeto/model/ListaDeCompraTest.java
@@ -0,0 +1,149 @@
+package com.projeto.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ListaDeCompraTest {
+
+	@Test
+	public void testListaDeCompra() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdicionaCompraALista() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAtualizaCompraDeLista() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetItemLista() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBuscaTodosItens() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPesquisaCompraEmLista() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFinalizar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsFinalizada() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeletaCompraDeLista() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDescritor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPossuiCompra() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDataFormatada() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDataTextual() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCompras() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCompras() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetValorFinal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetValorFinal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testContemItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCompra() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdicionaItens() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLocal() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSugereMelhorEstabelecimento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetItens() {
+		fail("Not yet implemented");
+	}
+
+}
